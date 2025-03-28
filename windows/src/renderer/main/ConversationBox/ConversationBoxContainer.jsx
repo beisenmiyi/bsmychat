758f071b5dcef5c8ffe95fdd5a4298cb60e390c7
beisenmiyi <3502771302@qq.com>
2025-03-29 00:04:26 +0800

    Windows端搜索联系人功能

--- windows/src/renderer/main/ConversationBox/ConversationBoxContainer.jsx
+++ windows/src/renderer/main/ConversationBox/ConversationBoxContainer.jsx
@@ -1,16 +1,19 @@
-import WindowHeader from "../../../components/WindowHeader/WindowHeader"
 import styles from "./ConversationBoxContainer.module.scss"
+import SearchContactResult from "./SearchContactResult/SearchContactResult";
 
-export default function ConversationBoxContainer() {
+export default function ConversationBoxContainer({ ConversationBoxSelectedId, searchContactResultData }) {
+
+    const items = [
+        <SearchContactResult searchContactResultData={searchContactResultData} />
+    ];
+
     return (
         <div className={styles.root}>
-            <div className={styles.conversationHeader}>
-                <div className={styles.conversationName}>会话名称</div>
-                <div className={styles.windowHeaderAndOther}>
-                    <WindowHeader />
-                    <div className={styles.other}></div>
-                </div>
-            </div>
+            {
+                items.map((item, index) => (
+                    index === ConversationBoxSelectedId ? <div key={index}>{item}</div> : null
+                ))
+            }
         </div>
     )
 }
